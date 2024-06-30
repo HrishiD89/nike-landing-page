e99e6b89d9d14dcfd22352403415f2213c27ca45
Hrishikesh Kalita <95520030+HrishiD89@users.noreply.github.com>
2024-07-01 00:20:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { Hero,
+  SuperQuality,
+  PopularProducts,
+  Footer,
+  Services,
+  CustomerReviews,
+  Subscribe,
+  SpecialOffers } from './sections'
+
+import Nav from './components/Nav'
+
+export default function App() {
+  return (
+    <main className="relative">
+      <Nav/>
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero/>
+      </section>
+      <section className="padding">
+        <PopularProducts/>
+      </section>
+      <section className="padding">
+        <SuperQuality/>
+      </section>
+      <section className="padding py-10">
+        <Services/>
+      </section>
+      <section className="padding">
+        <SpecialOffers/>
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews/>
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe/>
+      </section>
+      <section className="padding bg-black padding-x padding-t pb-8">
+        <Footer/>
+      </section>
+
+    </main>
+  )
+}
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero'
+import SuperQuality from './SuperQuality'
+import Footer from './Footer'
+import Services from './Services'
+import CustomerReviews from './CustomerReviews'
+import Subscribe from './Subscribe'
+import SpecialOffers from './SpecialOffers'
+import PopularProducts from './PopularProducts'
+
+export {
+    Hero,
+    SuperQuality,
+    PopularProducts,
+    Footer,
+    Services,
+    CustomerReviews,
+    Subscribe,
+    SpecialOffers
+}
